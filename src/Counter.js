53d92a6e76fa75839101175eb591c792d596fd58
Ikorian <138433077+Ikorian@users.noreply.github.com>
2023-07-04 09:40:49 +0300

    feat: Counter

---
+++ src/Counter.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+class Counter extends Component
+{
+    constructor(props){
+        super(props);
+        this.state={
+            count:0
+        };
+    }
+    incrementCount=()=>{
+        this.setState(prevState=>({count: prevState.count+1}))
+    }
+    decrementCount=()=>{
+        this.setState(prevState=>({count: prevState.count-1}))
+    }
+    render(){
+        return(
+            <div>
+                <h1>Счётчик: {this.state.count}</h1>
+                <button onClick={this.incrementCount}>+</button>
+                <button onClick={this.decrementCount}>-</button>
+            </div>
+        )
+    }
+}
+export default Counter;
